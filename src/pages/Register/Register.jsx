a4a9524504567f0ea22093aaae10fb4d6b3e5b38
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-19 00:08:36 +0600

    added registration functionality along with updating name and photo url values using firebase in the register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import loginImg from '../../assets/images/login-2.png';
-import { BiLogIn } from "react-icons/bi";
-import { FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Register = () => {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const { createUser, updateUser } = useContext(AuthContext);
+
+    const handleRegister = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        // console.log(name, photo, email, password);
+        setError('');
+        setSuccess('');
+
+        createUser(email, password)
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                setSuccess('User Registered Successfully...');
+
+                handleUpdateUser(createdUser, name, photo);
+
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+            });
+    }
+
+    const handleUpdateUser = (user, name, photo) => {
+        updateUser(user, name, photo)
+            .then(() => { })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+            });
+    }
+
     return (
         <div className="hero pt-7 pb-20 bg-base-100">
             <div className="hero-content flex-col lg:flex-row gap-8">
@@ -16,31 +59,35 @@ const Register = () => {
                         <div className="flex flex-col items-center text-center gap-5 mb-2">
                             <h3 className="text-3xl font-medium">Register</h3>
                         </div>
-                        <form>
+                        <form onSubmit={handleRegister}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-xl">Name</span>
                                 </label>
-                                <input type="text" name="name" placeholder="Name" className="input input-bordered" />
+                                <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-xl">Photo URL</span>
                                 </label>
-                                <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" />
+                                <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-xl">Email</span>
                                 </label>
-                                <input type="email" name="email" placeholder="Email" className="input input-bordered" />
+                                <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text text-xl">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="Password" className="input input-bordered" />
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
                             </div>
+
+                            <p className="font-medium text-lg text-red-500">{error}</p>
+                            <p className="font-medium text-lg text-green-600">{success}</p>
+
                             <div className="form-control mt-6">
                                 <input type="submit" value="Register" className="btn btn-warning text-lg capitalize" />
                             </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,14 +1,29 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUser = (user, name, photo) => {
+        return updateProfile(user, {
+            displayName: name, photoURL: photo
+        });
+    }
+
     const authInfo = {
         user,
         loading,
+        createUser,
+        updateUser,
     };
 
     return (
