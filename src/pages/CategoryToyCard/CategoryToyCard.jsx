aebb856f3f0ce009aefee762e620a22745c7c5c5
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-20 05:07:14 +0600

    showed toys based on sub category using react-tabs in the shop by category section of the home page. filtered toys based on sub category from all fetched toys from the server side

--- src/pages/CategoryToyCard/CategoryToyCard.jsx
+++ src/pages/CategoryToyCard/CategoryToyCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CategoryToyCard = () => {
+const CategoryToyCard = ({ toyInfo }) => {
+    const { _id, toyPhoto, toyName, price, rating } = toyInfo;
     return (
-        <div>
-            
+        <div className="card card-compact w-72 bg-base-100 shadow-xl">
+            <figure><img src={toyPhoto} alt="action figure toy" className="h-48"/></figure>
+            <div className="card-body">
+                <h2 className="card-title">Name: {toyName}</h2>
+                <p>Price: {price}</p>
+                <div className="card-actions justify-start">
+                    <Link to={`/toy/${_id}`}>
+                        <button className="btn btn-primary">View Details</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -2,15 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import './ShopByCategory.css';
+import CategoryToyCard from '../../CategoryToyCard/CategoryToyCard';
 
 const ShopByCategory = () => {
     const [subCategories, setSubCategories] = useState([]);
+    const [marvelToys, setMarvelToys] = useState([]);
+    const [dcToys, setDcToys] = useState([]);
+    const [narutoToys, setNarutoToys] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/subcategories')
             .then(res => res.json())
             .then(data => setSubCategories(data));
     }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/toys')
+            .then(res => res.json())
+            .then(data => {
+
+                const marvel = data.filter(toy => toy.subCategory === 'Marvel');
+                setMarvelToys(marvel);
+
+                const dc = data.filter(toy => toy.subCategory === 'DC');
+                setDcToys(dc);
+
+                const naruto = data.filter(toy => toy.subCategory === 'Naruto');
+                setNarutoToys(naruto);
+            });
+    }, [])
 
     return (
         <div>
@@ -26,15 +46,39 @@ const ShopByCategory = () => {
                         </Tab>)
                     }
                 </TabList>
-                
+
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <div className="my-10">
+                        {
+                            marvelToys.map(marvelToy => <CategoryToyCard
+                                key={marvelToy._id}
+                                toyInfo={marvelToy}
+                            >
+                            </CategoryToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className="my-10">
+                        {
+                            dcToys.map(dcToy => <CategoryToyCard
+                                key={dcToy._id}
+                                toyInfo={dcToy}
+                            >
+                            </CategoryToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                    <div className="my-10">
+                        {
+                            narutoToys.map(narutoToy => <CategoryToyCard
+                                key={narutoToy._id}
+                                toyInfo={narutoToy}
+                            >
+                            </CategoryToyCard>)
+                        }
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
