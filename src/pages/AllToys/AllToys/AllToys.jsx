1c614ec619460430b022d4b0fdf7d79345c3e468
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-21 03:34:56 +0600

    implemented search funtionality on all toys page

--- src/pages/AllToys/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys/AllToys.jsx
@@ -8,10 +8,30 @@ const AllToys = () => {
 
     const [allToys, setAllToys] = useState(loadedAllToys);
 
+    const handleSearch = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const searchFieldValue = form.toyname.value;
+
+        fetch(`http://localhost:5000/toys/search/toyname?name=${searchFieldValue}`)
+            .then(res => res.json())
+            .then(data => setAllToys(data));
+    }
+
     return (
         <div className="py-20">
             <h2 className="text-3xl font-semibold text-center">All Toys</h2>
-            <div className="overflow-x-auto max-w-7xl mx-4 lg:mx-auto mt-16">
+            <div class="max-w-7xl mx-4 lg:mx-auto flex justify-end items-center mt-7">
+                <form onSubmit={handleSearch}>
+                    <input class="px-4 py-2 border-2 border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        type="text" name="toyname" placeholder="Search By Toy Name" />
+                    <button class="px-4 py-[10px] bg-green-600 text-white rounded-r-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" type="submit">Search</button>
+                </form>
+            </div>
+
+            <div className="overflow-x-auto max-w-7xl mx-4 lg:mx-auto mt-14">
                 <table className="table w-full table-zebra border-2 text-center">
                     <thead>
                         <tr>
