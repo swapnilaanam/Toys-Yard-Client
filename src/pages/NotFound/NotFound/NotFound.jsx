e4992740f47777d7cecb7e1b521b2c9c4b7c1fb5
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-21 20:40:27 +0600

    added 404 page with an interesting jpg image and a back to home button

--- src/pages/NotFound/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound/NotFound.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFoundImage from '../../../assets/images/notfound.png';
+import useTitle from '../../../hooks/useTitle';
 
 const NotFound = () => {
+    useTitle('404')
+
     return (
-        <div>
-            <h2>Not Found</h2>
-        </div>
+        <div className="flex justify-evenly items-center flex-wrap min-h-screen px-10">
+            <div className="flex flex-col justify-center items-center w-2/4">
+                <div className="relative">
+                    <h1 class="text-9xl font-extrabold text-sky-500 tracking-widest">404</h1>
+                </div>
+                <div className="my-4">
+                    <p className="text-lg font-medium mt-2">
+                        Sorry The Page You Are Looking For Doesn't Exist...
+                    </p>
+                </div>
+                <Link to="/">
+                    <button className='btn btn-lg px-8 mt-5 bg-green-500 border-0 hover:bg-green-600'>
+                        Back To Home
+                    </button>
+                </Link>
+            </div>
+            <div className="w-full md:w-2/4 flex">
+                <img src={notFoundImage} alt="NotFound" style={{ maxHeight: '650px' }} />
+            </div>
+        </div >
     );
 };
 
